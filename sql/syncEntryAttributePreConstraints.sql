249191d7ad55141a98f3a1ff0689f8a1e8f184ed
Jonathan Zacharuk <jzacharuk@gmail.com>
2018-03-14 19:27:11 -0700

    Modified pre constraints to drop only if exists

--- sql/syncEntryAttributePreConstraints.sql
+++ sql/syncEntryAttributePreConstraints.sql
@@ -1,3 +1,3 @@
-ALTER TABLE universal.entry_attribute DROP CONSTRAINT entry_attribute_pkey;
-ALTER TABLE universal.entry_attribute DROP CONSTRAINT entry_attribute_attribute_id_fkey;
-ALTER TABLE universal.entry_attribute DROP CONSTRAINT entry_attribute_entry_id_fkey;
+ALTER TABLE universal.entry_attribute DROP CONSTRAINT IF EXISTS entry_attribute_pkey;
+ALTER TABLE universal.entry_attribute DROP CONSTRAINT IF EXISTS entry_attribute_attribute_id_fkey;
+ALTER TABLE universal.entry_attribute DROP CONSTRAINT IF EXISTS entry_attribute_entry_id_fkey;
--- sql/syncEntryAttributePreIndices.sql
+++ sql/syncEntryAttributePreIndices.sql
@@ -1,6 +1,6 @@
-DROP INDEX universal.idx_entry_attribute_attribute_id;
-DROP INDEX universal.idx_entry_attribute_code_system;
-DROP INDEX universal.idx_entry_attribute_code_value;
-DROP INDEX universal.idx_entry_attribute_date_value;
-DROP INDEX universal.idx_entry_attribute_entry_id;
-DROP INDEX universal.idx_entry_attribute_text_value;
+DROP INDEX IF EXISTS universal.idx_entry_attribute_attribute_id;
+DROP INDEX IF EXISTS universal.idx_entry_attribute_code_system;
+DROP INDEX IF EXISTS universal.idx_entry_attribute_code_value;
+DROP INDEX IF EXISTS universal.idx_entry_attribute_date_value;
+DROP INDEX IF EXISTS universal.idx_entry_attribute_entry_id;
+DROP INDEX IF EXISTS universal.idx_entry_attribute_text_value;
