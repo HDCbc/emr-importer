1b2eddb53f8c583ad2ea269187ded01791639f44
Daniel Egli <daniel@egli.ca>
2018-05-18 12:48:29 -0700

    Removed the attribute_id constraint from the entry_attribute table

--- sql/syncEntryAttributePreConstraints.sql
+++ sql/syncEntryAttributePreConstraints.sql
@@ -1,3 +1,4 @@
 ALTER TABLE universal.entry_attribute DROP CONSTRAINT IF EXISTS entry_attribute_pkey;
+-- This constraint is no longer created. Leaving the drop for endpoints that may still have it.
 ALTER TABLE universal.entry_attribute DROP CONSTRAINT IF EXISTS entry_attribute_attribute_id_fkey;
 ALTER TABLE universal.entry_attribute DROP CONSTRAINT IF EXISTS entry_attribute_entry_id_fkey;
