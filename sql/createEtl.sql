83ccf47a8c1cce816d17a3f41e6ece8056b7f33b
Daniel Egli <daniel@egli.ca>
2018-05-24 11:01:25 -0700

    Changed emr_effective_date to timestamptz datatype

--- sql/createEtl.sql
+++ sql/createEtl.sql
@@ -16,7 +16,7 @@ ALTER TABLE etl.patient SET (autovacuum_enabled = false, toast.autovacuum_enable
 CREATE UNLOGGED TABLE IF NOT EXISTS etl.patient_state (emr_patient_id text, state text, effective_date timestamp with time zone, emr_reference text);
 ALTER TABLE etl.patient_state SET (autovacuum_enabled = false, toast.autovacuum_enabled = false);
 
-CREATE UNLOGGED TABLE IF NOT EXISTS etl.entry_attribute (source_table text, attribute_id numeric(6,3), emr_entry_id text, code_system text, code_value text, text_value text, date_value date, boolean_value boolean, numeric_value numeric(18,6), emr_id text, effective_date date, emr_reference text);
+CREATE UNLOGGED TABLE IF NOT EXISTS etl.entry_attribute (source_table text, attribute_id numeric(6,3), emr_entry_id text, code_system text, code_value text, text_value text, date_value date, boolean_value boolean, numeric_value numeric(18,6), emr_id text, effective_date timestamptz, emr_reference text);
 ALTER TABLE etl.entry_attribute SET (autovacuum_enabled = false, toast.autovacuum_enabled = false);
 
 CREATE UNLOGGED TABLE IF NOT EXISTS etl.entry_state (source_table text, emr_entry_id text, state text, effective_date timestamp with time zone, emr_reference text);
